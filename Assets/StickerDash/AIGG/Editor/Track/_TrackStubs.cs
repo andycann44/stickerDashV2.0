3990563b57c0705701500d9c9bfcebd91ba8f769
Andy Cannell <andycann44@yahoo.com>
2025-10-01 20:49:36 +0100

    chore(track): stub Track namespace/types to satisfy references

---
+++ Assets/StickerDash/AIGG/Editor/Track/_TrackStubs.cs
@@ -0,0 +1,9 @@
+#if UNITY_EDITOR
+using UnityEditor; using UnityEngine;
+namespace Aim2Pro.AIGG.Track {
+  public class TrackGenV2Window : EditorWindow {}
+  public class TrackGeneratorWindow : EditorWindow {}
+  public static class CanonicalRunner {}
+  public static class TrackSceneBuilder {}
+}
+#endif
