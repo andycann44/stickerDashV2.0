c5f1fc2908d39916a790d343c5120f6299f61326
Andy Cannell <andycann44@yahoo.com>
2025-10-05 20:10:15 +0100

    chore: parked stub for TrackGenV2Window to bypass stale CS2001

---
+++ Assets/StickerDash/AIGG/Editor/Track/TrackGenV2Window.cs
@@ -0,0 +1,4 @@
+#if false
+// Parked stub: satisfies stale Unity compile lists; does nothing and defines no menu.
+namespace Aim2Pro.TrackCreator { internal static class TrackGenV2Window_Parked {} }
+#endif
